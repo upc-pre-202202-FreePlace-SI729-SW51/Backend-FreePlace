1308ac85d58f3fe55e9cf3edd1af61ea1f452393
Asura <u202020239@upc.edu.pe>
2022-10-29 14:58:33 -0500

    Initial commit

---
+++ src/main/java/com/acme/freeplace/profile/mapping/MappingConfiguration.java
@@ -0,0 +1,8 @@
+//Put here the entity mappers
+package com.acme.freeplace.profile.mapping;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("bookingMappingConfiguration")
+public class MappingConfiguration {
+}
