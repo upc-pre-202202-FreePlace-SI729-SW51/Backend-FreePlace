67692c47d7a94d82dacd4e5400ddef94f1ed8222
Asura <u202020239@upc.edu.pe>
2022-11-14 18:43:16 -0500

    feat(review) Add review section

---
+++ src/main/java/com/acme/freeplace/booking/resource/ReviewsResource.java
@@ -0,0 +1,15 @@
+package com.acme.freeplace.booking.resource;
+
+import com.acme.freeplace.booking.domain.model.entity.Reservation;
+
+import java.util.Date;
+
+public class ReviewsResource {
+    private Long id;
+
+    private Reservation reservation;
+
+    private String coment;
+
+    private Long starts;
+}
