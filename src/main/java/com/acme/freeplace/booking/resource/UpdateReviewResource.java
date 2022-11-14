1a6838906ced4c1d58088c867d3f0b2ec0982995
Asura <u202020239@upc.edu.pe>
2022-11-14 18:43:16 -0500

    feat(review) Add review section

---
+++ src/main/java/com/acme/freeplace/booking/resource/UpdateReviewResource.java
@@ -0,0 +1,28 @@
+package com.acme.freeplace.booking.resource;
+
+import com.acme.freeplace.booking.domain.model.entity.Reservation;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.util.Date;
+
+@Getter
+@Setter
+public class UpdateReviewResource {
+
+    private Long id;
+
+    @ManyToOne(fetch= FetchType.EAGER, optional = false)
+    @JoinColumn(name = "reservation_id",nullable = false)
+    @JsonIgnore
+    private Reservation reservation;
+
+    private String coment;
+    private Long strars;
+
+}
