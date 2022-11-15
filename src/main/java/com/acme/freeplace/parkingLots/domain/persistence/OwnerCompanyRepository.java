b1d37cca511848375ca9efa3bff00be52ed16d6b
Asura <u202020239@upc.edu.pe>
2022-11-15 02:19:21 -0500

    Delete

--- src/main/java/com/acme/freeplace/parkingLots/domain/persistence/OwnerCompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acme.freeplace.parkingLots.domain.persistence;
-
-import com.acme.freeplace.parkingLots.domain.model.entity.OwnerCompany;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OwnerCompanyRepository extends JpaRepository<OwnerCompany,Long> {
-}
