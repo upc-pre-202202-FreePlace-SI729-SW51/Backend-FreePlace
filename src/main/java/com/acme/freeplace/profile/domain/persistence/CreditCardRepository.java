86f649835b9207756d1308506a393ce4a4737623
Asura <u202020239@upc.edu.pe>
2022-11-16 00:56:40 -0500

    Delete

--- src/main/java/com/acme/freeplace/profile/domain/persistence/CreditCardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acme.freeplace.profile.domain.persistence;
-
-import com.acme.freeplace.profile.domain.model.entity.CreditCard;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CreditCardRepository extends JpaRepository<CreditCard,Long> {
-}
--- src/main/java/com/acme/freeplace/security/mapping/MappingConfiguration.java
+++
@@ -1,8 +0,0 @@
-//Put here the entity mappers
-package com.acme.freeplace.security.mapping;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration("securityMappingConfiguration")
-public class MappingConfiguration {
-}
